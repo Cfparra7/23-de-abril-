e402fccede6f3d2b5c8526b79fd5d476cc1c7b5a
Cfparra7 <90563604+Cfparra7@users.noreply.github.com>
2022-04-24 00:26:35 -0500

    Cambio de nombre CP

--- index.php
+++ index.php
@@ -15,7 +15,7 @@
                       <div class="col-md-8 col-md-offset-2">
                           <div class="panel panel-primary">
                               <div class="panel-heading">
-                                   Calculadora Basica SENA
+                                   Calculadora Basica SENA <strong>CP</strong>
                               </div>
                               <div class="panel-body">
                                   <div class="row">
