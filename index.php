5d4c4404d2eca0d5258593945c9c7b406e5080e8
Cfparra7 <90563604+Cfparra7@users.noreply.github.com>
2022-04-24 00:05:53 -0500

    varibale php

--- index.php
+++ index.php
@@ -21,17 +21,17 @@
                                   <div class="row">
                                       <div class="col-md-6">
                                           <label for="">Numero 1</label>
-                                          <input type="number" value="" class="form-control" name="numero1" id="numero1" required>
+                                          <input type="number" value="<?php print $_GET ['numero1']?>" class="form-control" name="numero1" id="numero1" required>
                                       </div>
                                     
                                       <div class="col-md-6">
                                           <label for="">Numero 2</label>
-                                          <input type="number" value="" class="form-control" name="numero2" id="numero1" required>
+                                          <input type="number" value="<?php print $_GET ['numero2']?>" class="form-control" name="numero2" id="numero1" required>
                                       </div>
                                   </div>
                                   <hr>
                                    <button type="submit" class="btn btn-primary pull-right">Sumar</button>
-                                   <button type="submit" class="btn btn-danger pull-right">Restar</button>
+                                  
                                 </div>
                           </div>
                       </div>
@@ -42,13 +42,18 @@
            <div class="row">
                <div class="col-md-8 col-md-offset-2">
                   <div class="alert alert-success">
-                    El resultado de la suma es: <strong>resultado</strong>
+                    El resultado de la suma es:<?php
+                    $valor1 = $_GET['numero1'];
+                    $valor2 = $_GET['numero2'];
+                   
+
+                    $suma = $valor1 + $valor2;
+
+                    echo "$suma";
+?> <strong>resultado</strong>
                   </div>
                </div>
            </div>
        </div>
 </body>
 </html>
-<?php
-    print('hello world');
-?>
